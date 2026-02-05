a0690e61cffba401f698f0e09cb01f8f015eedc4
Lucie Paradis <l.paradis04@gmail.com>
2026-02-05 10:56:08 +0000

    operationnal marhin correcte (integration shipping fees et ventilation ship cost log cost)

--- models/schema.yml/sales_margin/int_orders_operational.sql
+++ models/schema.yml/sales_margin/int_orders_operational.sql
@@ -1,41 +1,5 @@
-WITH purchase_cost_table AS (
-    SELECT 
-        products_id,
-        orders_id,
-        date_date,
-        SUM(quantity) AS quantity,
-        round(SUM(revenue),2) AS revenue,
-        -- Calcul du coût d'achat total par produit/commande
-        round (SUM(CAST(quantity AS FLOAT64) * CAST(purchase_price AS FLOAT64)),2) AS purchase_cost
-    FROM {{ ref('stg_gz_raw_data__sales') }}
-    INNER JOIN {{ ref('stg_gz_raw_data__product') }} USING (products_id)
-    GROUP BY products_id, orders_id, date_date
-),
-
-orders_aggregation AS (
-    SELECT 
-        orders_id,
-        count(products_id) as nb_orders,
-        date_date,
-        SUM(quantity) AS total_quantity,
-        round(SUM(revenue),2) AS revenue,
-        round(SUM(purchase_cost),2) AS purchase_cost,
-        round(SUM(CAST(ship_cost AS FLOAT64)),2) as ship_cost,
-        round(SUM(CAST(logcost AS FLOAT64)),2) as log_cost
-    FROM purchase_cost_table
-    INNER JOIN {{ ref('stg_gz_raw_data__ship') }} USING (orders_id)
-    GROUP BY orders_id, date_date
-),
-
-margin_table as (
-    SELECT 
-    *,
-    -- Calcul final de la marge 
-    round(CAST(revenue - purchase_cost AS FLOAT64),2) AS margin 
-FROM orders_aggregation)
-
 SELECT 
     *,
     -- Calcul final de la marge opérationnel
     round(CAST(margin - ship_cost - log_cost as FLOAT64),2) AS operational_margin 
-FROM margin_table
+FROM {{ ref('int_orders_margin') }}
