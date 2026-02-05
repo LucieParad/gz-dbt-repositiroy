c0db780ef2cba6393d156a5f4f10a3878adf471a
Lucie Paradis <l.paradis04@gmail.com>
2026-02-05 11:11:33 +0000

    mart perfect

--- models/schema.yml/mart.sql
+++ models/schema.yml/mart.sql
@@ -1,17 +1,33 @@
 WITH sum_table AS (
+
     SELECT 
+
         date_date,
+
         count(orders_id) as nb_order,
-        sum (revenue) as revenue, 
-        sum(operational_margin) as operational_margin,
-        sum(purchase_cost) as purshase_cost,
-        sum (shipping_fees) as shipping_fees,
-        sum(log_cost) as log_cost,
-        sum(total_quantity),        
+
+        round(sum (revenue),2) as revenue, 
+
+        round(sum(operational_margin),2) as operational_margin,
+
+        round(sum(purchase_cost),2) as purchase_cost,
+
+        round(sum (shipping_fee),2) as shipping_fees,
+
+        round(sum(log_cost),2) as log_cost,
+
+        round(sum(total_quantity),2) as nb_product,        
+
     FROM {{ ref('int_orders_operational') }}
-    Inner join {{ ref('stg_gz_raw_data__ship') }}
-    GROUP BY data_date
+
+    Inner join {{ ref('stg_gz_raw_data__ship') }} using(orders_id)
+
+    GROUP BY date_date)
+
+
 
 select *,
-safe_divide (revenue,nb_order)
+
+round(safe_divide (revenue,nb_order),2) as average_basket
+
 from sum_table
