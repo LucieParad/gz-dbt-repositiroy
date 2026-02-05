af6b0ab2ed239be9af0d7f29ec40016e9ad4523a
Lucie Paradis <l.paradis04@gmail.com>
2026-02-05 16:13:35 +0000

    margin percent

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,9 @@
 WITH purchase_cost_table AS (
-    SELECT 
+ SELECT 
         products_id,
         SUM(quantity) AS total_quantity,
         SUM(revenue) AS total_revenue,
+        -- Calcul du coût d'achat total
         SUM(CAST(quantity AS FLOAT64) * CAST(purchase_price AS FLOAT64)) AS purchase_cost
     FROM {{ ref('stg_gz_raw_data__sales') }}
     INNER JOIN {{ ref('stg_gz_raw_data__product') }} USING (products_id)
@@ -11,5 +12,8 @@ WITH purchase_cost_table AS (
 
 SELECT 
     *,
-    CAST(total_revenue - purchase_cost AS FLOAT64) AS margin 
+    -- Calcul de la marge brute
+    CAST(total_revenue - purchase_cost AS FLOAT64) AS margin,
+    -- Appel de ta macro pour le pourcentage de marge
+    {{ margin_percent('total_revenue', 'purchase_cost') }} AS margin_percent
 FROM purchase_cost_table
