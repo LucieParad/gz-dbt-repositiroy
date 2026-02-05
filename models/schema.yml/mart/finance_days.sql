a6d595271365199f43204b3d6cefae806e7fa482
Lucie Paradis <l.paradis04@gmail.com>
2026-02-05 11:38:54 +0000

    table

--- models/schema.yml/mart/finance_days.sql
+++ models/schema.yml/mart/finance_days.sql
@@ -1,33 +1,33 @@
-WITH sum_table AS (
+ {{ config(materialized='table') }}
 
-    SELECT 
+with
+    sum_table as (
 
-        date_date,
+        select
 
-        count(orders_id) as nb_order,
+            date_date,
 
-        round(sum (revenue),2) as revenue, 
+            count(orders_id) as nb_order,
 
-        round(sum(operational_margin),2) as operational_margin,
+            round(sum(revenue), 2) as revenue,
 
-        round(sum(purchase_cost),2) as purchase_cost,
+            round(sum(operational_margin), 2) as operational_margin,
 
-        round(sum (shipping_fee),2) as shipping_fees,
+            round(sum(purchase_cost), 2) as purchase_cost,
 
-        round(sum(log_cost),2) as log_cost,
+            round(sum(shipping_fee), 2) as shipping_fees,
 
-        round(sum(total_quantity),2) as nb_product,        
+            round(sum(log_cost), 2) as log_cost,
 
-    FROM {{ ref('int_orders_operational') }}
+            round(sum(total_quantity), 2) as nb_product,
 
-    Inner join {{ ref('stg_gz_raw_data__ship') }} using(orders_id)
+        from {{ ref("int_orders_operational") }}
 
-    GROUP BY date_date)
+        inner join {{ ref("stg_gz_raw_data__ship") }} using (orders_id)
 
+        group by date_date
+    )
 
-
-select *,
-
-round(safe_divide (revenue,nb_order),2) as average_basket
+select *, round(safe_divide(revenue, nb_order), 2) as average_basket
 
 from sum_table
